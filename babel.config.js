e64c99e1e8247ee5df810c0451264d148941ee93
drinchev <kaloyan.drinchev.ext@nokia.com>
2025-08-09 22:01:01 +0300

    fix: update correct lib versions

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,10 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [['babel-preset-expo', { jsxImportSource: 'nativewind' }]],
+    presets: [['babel-preset-expo', { 
+      jsxImportSource: 'nativewind',
+      unstable_transformImportMeta: true // Fix for zustand import.meta issue
+    }]],
     plugins: [
       ['inline-import', { extensions: ['.sql'] }], // Support for importing .sql files
     ],
